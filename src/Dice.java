c80612a3597f4a03e8f32a604417c4ac160d81a0
NikolajWeinell <58971957+Weinell@users.noreply.github.com>
2021-05-12 10:14:58 +0200

    Update Dice.java

--- src/Dice.java
+++ src/Dice.java
@@ -1,6 +1,21 @@
 public class Dice {
 
 
+    public int[] diceArray(int numDice){
 
+        for(int i = 0; i < numDice; i++)
+        {
+            fiveDice[i] = diceRoll();
+        }
+        return fiveDice;
+    }
+
+    public int diceRoll()
+    {
+        int diceRoll = (int)(Math.random()*6+1);
+        return diceRoll;
+    }
+
+    
 
 }
