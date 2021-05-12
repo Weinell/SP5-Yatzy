5580c02787d86ef4c498d0c892097778d92b92ef
JonasRex <78900583+JonasRex@users.noreply.github.com>
2021-05-12 11:17:00 +0200

    Update Combinations.java

--- src/Combinations.java
+++ src/Combinations.java
@@ -29,5 +29,18 @@ public class Combinations {
         }
     }
 
+    public int three(int[] dice) {
+        int sum = 0;
+        int[] tempArr = new int[6];
+
+        for(int i = 1; i < dice.length; i++) {
+
+            tempArr[dice[i-1]] = dice[i];
+        }
+
+        System.out.println(tempArr);
+        return sum;
+    }
+
 
 }
