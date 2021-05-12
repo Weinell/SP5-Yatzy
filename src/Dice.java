722cf17ee2c27ab7e151f808e9a92ced6d1834cd
cph-mb780 <78900586+cph-mb780@users.noreply.github.com>
2021-05-12 09:40:10 +0200

    Dice experiment added

--- src/Dice.java
+++ src/Dice.java
@@ -1,6 +1,62 @@
 public class Dice {
+    int[] fiveDice = new int[5];
+
+    //Rewrite with for loop and use input from user -
+    public void changeDice(int a, int b, int c, int d, int e)
+    {
+        if(a ==1)
+        {
+            fiveDice[0] = diceRoll();
+        }else
+        {
+            System.out.println("Dice 1 is unchanged");
+        }
+        if(b == 1)
+        {
+            fiveDice[1] = diceRoll();
+        }else
+        {
+            System.out.println("Dice 2 is unchanged");
+        }
+        if(c == 1)
+        {
+            fiveDice[2] = diceRoll();
+        }else
+        {
+            System.out.println("Dice 3 is unchanged");
+        }
+        if(d == 1)
+        {
+            fiveDice[3] = diceRoll();
+        }else
+        {
+            System.out.println("Dice 4 is unchanged");
+        }
+        if(e == 1)
+        {
+            fiveDice[4] = diceRoll();
+        }else
+        {
+            System.out.println("Dice 5 is unchanged");
+        }
 
 
+    }
 
+
+    public int[] diceArray(int numDice){
+
+        for(int i = 0; i < numDice; i++)
+        {
+            fiveDice[i] = diceRoll();
+        }
+        return fiveDice;
+    }
+
+    public int diceRoll()
+    {
+        int diceRoll = (int)(Math.random()*6+1);
+        return diceRoll;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        // write your code here
+        Dice dice = new Dice();
+        int[] diceRoll = dice.diceArray(5);
+        System.out.println(Arrays.toString(diceRoll));
+//        for (int i = 0; i < 10; i ++) {
+//            int num = dice.diceRoll();
+//
+//            System.out.println(num);
+//        }
 
-        
-
+        dice.changeDice(0,0,1,1,0);
+        System.out.println(Arrays.toString(diceRoll));
     }
 }
