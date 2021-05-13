55354c9c95878ac76d39d205f1f77b8139dc1661
JonasRex <78900583+JonasRex@users.noreply.github.com>
2021-05-13 14:21:52 +0200

    Update to the 3,4,5 of a kind method

--- src/Combinations.java
+++ src/Combinations.java
@@ -4,10 +4,10 @@ public class Combinations {
 
     //Scanner s = new Scanner();
 
-    public int singles(int[] dice, int n) {
+    public int singles(int[] diceRoll, int n) {
         int sum = 0;
 
-        for (int die : dice) {
+        for (int die : diceRoll) {
 
             if (die == n) {
 
@@ -17,42 +17,50 @@ public class Combinations {
         return sum;
     }
 
-    public int[] sortingDice(int[] dice) {
 
-        int[] tempArr = new int[6];
+    public int nOfAKind(int[] diceRoll, int n) {    // n er antal 'ens' vi checker for. F.eks. 3 ens.
 
-        for (int value : dice) {
+        int[] arr = sortingDice(diceRoll);    // Først sortere denne metode de fem terninge kast og smider dem ind i denne temp array.
+                                                // Se eventuelt sortingDice metoden længere nede.
 
-            tempArr[value - 1] += value;
+        for (int i = 1; i < arr.length + 1; i++) {  // arr består af 6 tal. Værdierne fra 1 til 6 og hvor mange point
+                                                    // de hver især har opnået ved diceRoll. F.eks 3 femmer: arr[4] = 15
 
+            if (arr[i - 1] == i*n) {     // i er sat til 1 (normalt 0), som repræsentere antal øjne på terningen.
+                                         // Hvis arr[5-1] == 5 * 3 (sandt i dette tilfælde). Så:
+                return i * n;    // Returnes værdien for i(antal øjne) gange med n(antal ens).   5 * 3 = 15
+
+            } else if (arr[i - 1] == i*(n+1)) {   // Dette checker hvis der derimod var 4 femmer, men vi stadig kun søger 3 ens i dette tilfælde.
+
+                return i * n;             // metoden skal stadig kun returner 5 * 3 = 15. Da vi ønsker at udfylde feltet for 3 ens.
+
+            } else if (arr[i - 1] == i*(n+2)) {   // Og her er det for 5 femmer, men vi stadig kun søger 3 ens i dette tilfælde.
+
+                return i * n;             // metoden skal stadig kun returner 5 * 3 = 15. Da vi ønsker at udfylde feltet for 3 ens.
+            }
         }
-
-        for (int n:tempArr) {
-            //System.out.println(n);
-        }
-
-        return tempArr;
+        return 0;
     }
 
-    public int nOfAKind(int[] dice, float n)   {
+    public int[] sortingDice(int[] unSortedDice) {
 
-        int sum = 0;
-        int[] tempArr = sortingDice(dice);
+        // eksempel på sortering:  int[]={2,2,6,6,4};
+        // arr[0] = 0     (1'ere)
+        // arr[1] = 4     (2'ere)
+        // arr[2] = 0     (3'ere)
+        // arr[3] = 4     (4'ere)
+        // arr[4] = 0     (5'ere)
+        // arr[5] = 12    (6'ere)
 
-        for (int i = 0; i < tempArr.length; i++) {
+        int[] arr = new int[6];
 
-            if (tempArr[i]/n == i+1) {
-                sum = tempArr[i];
-            }
-            else if (tempArr[i]/4 == i+1)    {
-                sum = tempArr[i]-i-1;
-            }
-            else if (tempArr[i]/5 == i+1)    {
-                sum = tempArr[i]-(i*2)-1;
-            }
+        for (int value : unSortedDice) {   // Hver terningkast har en value fra 1 til 6.
+
+            arr[value - 1] += value;  // eksempel unSortedDice[2] = 6. Derfor: arr[6-1] += 6
 
         }
-        return sum;
+
+        return arr;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,17 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Dice dice = new Dice();
-        int[] diceRoll = dice.diceArray(5);
-        System.out.println(Arrays.toString(diceRoll));
+//        Dice dice = new Dice();
+//        int[] diceRoll = dice.diceArray(5);
+//        System.out.println(Arrays.toString(diceRoll));
 //        for (int i = 0; i < 10; i ++) {
 //            int num = dice.diceRoll();
 //
 //            System.out.println(num);
 //        }
 
-        dice.changeDice(0,0,1,1,0);
-        System.out.println(Arrays.toString(diceRoll));
+//        dice.changeDice(0,0,1,1,0);
+//        System.out.println(Arrays.toString(diceRoll));
 
         Combinations c = new Combinations();
 
@@ -26,12 +26,22 @@ public class Main {
 //        System.out.println(c.singles(diceRoll, 5));
 //        System.out.println(c.singles(diceRoll, 6));
 
-        int[] diceTest = new int[]{1,1,1,1,1};
+//        int[] diceTest = new int[]{1,1,1,1,1};
+//        int[] diceTest = new int[]{2,2,2,2,2};
+//        int[] diceTest = new int[]{3,3,3,3,3};
+//        int[] diceTest = new int[]{4,4,4,4,4};
+        int[] diceTest = new int[]{5,5,5,5,5};
+//        int[] diceTest = new int[]{6,6,6,6,6};
 
         //c.sortingDice(diceRoll);
-        System.out.println(c.nOfAKind(diceTest, 3));
-        System.out.println(c.nOfAKind(diceTest, 4));
-        System.out.println(c.nOfAKind(diceTest, 5));
+        System.out.println("3 ens: " + c.nOfAKind(diceTest, 3));
+        System.out.println("4 ens: " + c.nOfAKind(diceTest, 4));
+        System.out.println("5 ens: " + c.nOfAKind(diceTest, 5));
+
+
+//        System.out.println("3 ens: " + c.nOfAKind(diceRoll, 3));
+//        System.out.println("4 ens: " + c.nOfAKind(diceRoll, 4));
+//        System.out.println("5 ens: " + c.nOfAKind(diceRoll, 5));
 
     }
 }
