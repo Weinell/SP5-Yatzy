9c438b42ae515ff0dc7d6a4d69194f8a9fb0eb4e
Madsen79 <78900586+cph-mb780@users.noreply.github.com>
2021-05-14 11:04:22 +0200

    Working on menu

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,4 +1,38 @@
-public class GameEngine {
+import java.util.Scanner;
 
+public class GameEngine {
+    Dice dice = new Dice();
+
+
+    public int getUserInput(String msg)
+    {
+        System.out.println(msg);
+        Scanner scan = new Scanner(System.in);
+        int input = scan.nextInt();
+        return input;
+    }
+
+    public void UI()
+    {
+        switch (menu())
+        {
+            case 1:
+                System.out.println("Start new game");
+                break;
+            case 2:
+                System.out.println("Continue game");
+            case 3:
+                System.out.println("Exit");
+            case 4:
+                dice.
+        }
+    }
+
+    public int menu()
+    {
+        //System.out.println("Welcome to Yatzy! Make your choice: \n 1. Start new game \n 2. Continue game \n 3. Exit");
+        int menuChoice = getUserInput("Welcome to Yatzy! Make your choice: \n 1. Start new game \n 2. Continue game \n 3. Exit \n 4. Roll Dice");
+        return menuChoice;
+    }
 
 }
