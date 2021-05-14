1055cdc3e5ad9180e63afb438686e0d46d22220a
NikolajWeinell <58971957+Weinell@users.noreply.github.com>
2021-05-14 10:23:32 +0200

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,17 @@
 public class Player {
 
 
+    String name;
+    int score;
+    int ID;
+
+    public Player(String name, int score, int ID) {
+
+        this.name = name;
+        this.score = score;
+        this.ID = ID;
+    }
+
+    
 
 }
