29f411d6ceea54f27442c8fbbb28ab4328160258
cph-mb780 <78900586+cph-mb780@users.noreply.github.com>
2021-05-17 14:01:11 +0200

    Update GameEngine.java

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -187,7 +187,7 @@ public class GameEngine {
             //fiveDice[diceNumToChange-1] = new Dice();
             fiveDice[diceNumToChange-1].setValue(Dice.diceRoll());
         }
-        turnsLeft = 0;
+        turnsLeft--;
     }
 
 
