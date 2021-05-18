53271efc4a66f05b7301ec58df7203b68f58814b
cph-mb780 <78900586+cph-mb780@users.noreply.github.com>
2021-05-18 11:09:45 +0200

    Added TODO comments

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        //TODO: Highscore: board and load / save highscore
+        //TODO: GUI
         GameEngine ge = new GameEngine();
         ge.run();
     }
